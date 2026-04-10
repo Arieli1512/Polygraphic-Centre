f7def72cb1ed6c6b172398ffe45bd6e474f6e716
Aleksander Wardziński <aleks.wardzinski@gmail.com>
2026-04-10 21:07:08 +0200

    Init

---
+++ backend/src/main/java/com/drobnyd/drobnyd/DrobnydApplication.java
@@ -0,0 +1,13 @@
+package com.drobnyd.drobnyd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrobnydApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrobnydApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/drobnyd/drobnyd/DrobnydApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drobnyd.drobnyd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrobnydApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
