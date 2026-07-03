647c90fbf36c1daa78d557d315e36e2b13e5d467
Michał Wieczorek <m1998.wieczorek@gmail.com>
2026-07-03 18:38:52 +0200

    Add custom validation: printing point location must be in Poland

--- backend/src/main/java/com/drobnyd/drobnyd/printingpoint/PrintingPointValidator.java
+++ backend/src/main/java/com/drobnyd/drobnyd/printingpoint/PrintingPointValidator.java
@@ -1,11 +1,25 @@
 package com.drobnyd.drobnyd.printingpoint;
 
+import com.drobnyd.drobnyd.api.ApiFieldError;
 import com.drobnyd.drobnyd.printingpoint.dto.PrintingPointRequest;
 import com.drobnyd.drobnyd.validation.RequestValidator;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class PrintingPointValidator extends RequestValidator<PrintingPointRequest> {
+
+    @Override
+    protected void validateCustom(PrintingPointRequest request, List<ApiFieldError> errors) {
+        if (!"poland".equalsIgnoreCase(request.country().trim())) {
+            errors.add(new ApiFieldError(
+                "country",
+                "must be Poland",
+                "Punkt druku musi znajdować się w Polsce."
+            ));
+        }
+    }
 
     @Override
     protected String resolveUserHint(String fieldName, String constraintCode) {
